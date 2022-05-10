5635d7fb9f0398a1c8b2adaad59f765f1b81e7ce
Ella Braide <88412294+Missbraide@users.noreply.github.com>
2022-05-10 14:12:45 -0700

    init commit

---
+++ src/main/java/org/hibernate/models/Employee.java
@@ -0,0 +1,33 @@
+package org.hibernate.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Employee {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @NonNull
+    @Column(length = 150,unique = true,nullable = false)
+    String name;
+    @NonNull
+    double salary;
+
+    @ToString.Exclude
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name = "employee_project",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "projects_id"))
+    Set<Project> projects = new LinkedHashSet<>();
+
+}
